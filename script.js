04d2d0286ac44f11e9930e2c9fc0a0dc5441efd8
bismilahdestia <160644749+bismilahdestia@users.noreply.github.com>
2025-09-01 10:04:03 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Placeholder JS file for GitHub Pages
-// Add interactivity here if needed later
-
-console.log("No DPR site loaded successfully.");
+// Smart minimal script
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Smart No DPR site is running ✅");
+});
